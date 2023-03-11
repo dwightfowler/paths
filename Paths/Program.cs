e18f33460d607592d75f27469011c7a3513fc565
Dwight Fowler <37718618+dwightfowler@users.noreply.github.com>
2023-03-11 09:51:06 -0500

    Initial check-in

---
+++ Paths/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Paths
+{
+    class Program
+    {
+        private static List<string> foundPaths = new List<string>();
+
+        static void Main(string[] args)
+        {
+            ConsoleColor forecolor = Console.ForegroundColor;
+            Regex setvar = new Regex("%[^%]+%");
+            string paths;
+            try
+            {
+                Console.ForegroundColor = forecolor;
+                Console.WriteLine("System Paths");
+                paths = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine);
+                printPaths(paths);
+
+                Console.ForegroundColor = forecolor;
+                Console.WriteLine("User Paths");
+                paths = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User);
+                printPaths(paths);
+            }
+            finally
+            {
+                Console.ForegroundColor = forecolor;
+                Console.Write(" ");
+            }
+        }
+
+        private static void printPaths(string paths)
+        {
+            Regex setvar = new Regex("%[^%]+%");
+            string[] pathList = paths.Split(';');
+            foreach (String path in pathList)
+            {
+                if (path == "")
+                    continue;
+
+                ConsoleColor color = ConsoleColor.DarkGreen;
+                if (DuplicatesExist(path))
+                {
+                    color = ConsoleColor.DarkYellow;
+                }
+
+                if (Directory.Exists(path))
+                {
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(path);
+                    continue;
+                }
+
+                color = ConsoleColor.DarkRed;
+                Match m = setvar.Match(path);
+                if (m.Length <= 0)
+                {
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(path);
+                    continue;
+                }
+
+                string sVar = path.Substring(m.Index, m.Length);
+                string sVarVal = sVar.Trim('%');
+                sVarVal = Environment.GetEnvironmentVariable(sVarVal);
+                bool varExists = !String.IsNullOrEmpty(sVarVal);
+                color = varExists ? ConsoleColor.DarkYellow : ConsoleColor.DarkRed;
+                Console.ForegroundColor = color;
+                Console.Write($"{path} = ");
+                if (varExists)
+                {
+                    sVar = path.Replace(sVar, sVarVal);
+                    color = Directory.Exists(sVar) ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed;
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(sVar);
+                }
+                else
+                    Console.WriteLine($"Environment var does not exist %{sVar}%: {sVarVal}");
+            }
+        }
+
+        private static bool DuplicatesExist(string path)
+        {
+            int idxFound = foundPaths.BinarySearch(path);
+            bool exists = idxFound >= 0;
+            if (!exists)
+            {
+                foundPaths.Add(path);
+                foundPaths.Sort();
+            }
+            return exists;
+        }
+    }
+}
